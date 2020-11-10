2e95d6d792819b6e1f4a5b8563618b568f39a179
Lucas <lucas.odonnelll@gmail.com>
2020-11-09 20:10:04 -0800

    added new tweet and sign up button/updated routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_012403) do
+ActiveRecord::Schema.define(version: 2020_11_10_034203) do
 
   create_table "chirps", force: :cascade do |t|
     t.text "chirp"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
